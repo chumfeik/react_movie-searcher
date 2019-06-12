acf5e468227d802fc411149a8da130d5becfe7ca
SirLime <jook11@ukr.net>
2019-06-13 00:39:17 +0300

    More info for Shows

--- src/Components/Show.js
+++ src/Components/Show.js
@@ -2,14 +2,31 @@ import React from 'react';
 
 const Show = props => {
   const data = props.data;
+
   return (
-    <figure>
-      <img
-        src={`http://image.tmdb.org/t/p/w154/${data.poster_path}`}
-        alt={`${data.title || data.name}`}
-      />
-      <figcaption>{data.title || data.name}</figcaption>
-    </figure>
+    <div className="card">
+      <figure>
+        <img
+          src={`http://image.tmdb.org/t/p/w154/${data.poster_path}`}
+          alt={`${data.title || data.name}`}
+        />
+        <figcaption>{data.title || data.name}</figcaption>
+      </figure>
+      <div className="description">
+        <span>{data.media_type}</span>
+        <p>{data.overview}</p>
+      </div>
+      <span className="date">{data.release_date || data.first_air_date}</span>
+      <div className="additional_info">
+        <div className="genres">
+          Genres:
+          <span>first</span>
+          <span>second</span>
+          <span>third</span>
+        </div>
+        <span>Rating: {data.vote_average}</span>
+      </div>
+    </div>
   );
 };
 
--- src/Components/styles/ResultsListStyles.js
+++ src/Components/styles/ResultsListStyles.js
@@ -2,27 +2,46 @@ import styled from 'styled-components';
 
 const ResultsContainer = styled.div`
   display: grid;
-  margin: 1rem;
-  grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr));
   grid-gap: 1rem;
-  figure {
-    position: relative;
-    margin: 0 1rem;
-    padding: 0.5rem;
+  margin: 1rem;
+  .card {
+    display: grid;
+    grid-template-columns: 184px auto;
+    grid-template-rows: 1fr 3rem;
+    padding-top: 1rem;
     background-color: #ff6;
     border: 1px solid #aaa;
     border-radius: 0.5rem;
+  }
+  figure {
     text-align: center;
   }
   figcaption {
     padding-top: 1rem;
     font-size: 1.2rem;
   }
-  > span {
-    grid-column: 1/-1;
-    margin: 0 auto;
+  .description {
+    margin: 0 1rem;
+    position: relative;
+    > span {
+      display: block;
+      margin-bottom: 1rem;
+      font-size: 1.2rem;
+      text-transform: capitalize;
+      text-align: right;
+    }
+  }
+  .date {
+    margin: auto;
+  }
+  .additional_info {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem;
+  }
+  .genres span {
+    padding: 0.25rem;
   }
 `;
-
 
 export { ResultsContainer };
