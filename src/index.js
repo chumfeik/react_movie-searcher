f050e70539c0ec196cb77b5f422cceaa4763008d
SirLime <jook11@ukr.net>
2019-08-08 14:10:37 +0300

    Extract store logic to configureStore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import reducers from './reducers';
+import { store } from './configureStore';
 import App from './App';
-
-const store = createStore(reducers);
 
 render(
   <Provider store={store}>
